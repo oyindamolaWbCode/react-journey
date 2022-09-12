d68019f4270b7aaba22def0cb8ab83d30622a3f3
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-09-12 16:04:12 +0200

    jsx page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+// import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('root');
+// const root = createRoot(root);
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const page = (
+  <div>
+    <h1>My awesome website in React</h1>
+    <p>Reasons I love React</p>
+    <ol>
+      <li>Its composable</li>
+      <li>Its is declarative</li>
+      <li>Its a hirable skill</li>
+      <li>Its actively maintained by skilled people</li>
+    </ol>
+  </div>
+)
+
+ReactDOM.render(page, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
